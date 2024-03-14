810f764cbbe69144e1a0f6398fe7e598a2c84a60
Akhi111 <acharekarakhil@gamil.com>
2024-03-14 14:55:14 +0530

    2nd - UI design part completed

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,16 @@
+import Convers from "./Convers";
+const Conversations = () => {
+  return (
+    <div className='py-2 flex flex-col overflow-auto'>
+      <Convers />
+      <Convers />
+      <Convers />
+      <Convers />
+      <Convers />
+      <Convers />
+      
+    </div>
+  )
+}
+
+export default Conversations;
---
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,24 @@
+import { BiLogOut} from "react-icons/bi";
+
+const LogoutButton = () => {
+  return (
+    <div className="mt-auto">
+    <BiLogOut className='w-6 h-6 text-black cursor-pointer' />
+    </div>
+  )
+}
+
+export default LogoutButton;
+
+// Starter code for this file
+// import { BiLogOut} from "react-icons/bi";
+
+// const LogoutButton = () => {
+//   return (
+//     <div className="mt-auto">
+//     <BiLogOut className='w-6 h-6 text-black cursor-pointer' />
+//     </div>
+//   )
+// }
+
+// export default LogoutButton;
